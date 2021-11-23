997812e2ddcb6a860648105f2743a8a879548ec5
unknown <estebamlo1975@gmail.com>
2021-11-23 19:28:05 -0300

    Creating new sass files

--- JS/CreatingElements.js
+++ JS/CreatingElements.js
@@ -9,15 +9,13 @@ const $documentBody = document.body;
 const scriptsSrcArray = ["../JS/AddingElements.js", "../JS/CSS-classes.js"];
 const $scriptsFragment = document.createDocumentFragment();
 //Adding scripts to fragment
-for (let i = 0; i < scriptsSrcArray; i++) {
+for (let i = 0; i < scriptsSrcArray.length; i++) {
   const $script = document.createElement("script");
-  $script.setAttribute("src", scriptsSrcArray[i]);
+  $script.setAttribute("src", `${scriptsSrcArray[i]}`);
   $scriptsFragment.appendChild($script);
 }
-
 //Adding JS scripts to html-
 $documentBody.appendChild($scriptsFragment);
-
 //------------------------------------------------------------------------------
 //Creating link tag to head
 const $linkToCSS = document.createElement("link");
