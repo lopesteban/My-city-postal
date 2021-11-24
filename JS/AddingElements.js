c74557a13a0ddd359a4f14ee5a466d6cbb3da3e4
unknown <estebamlo1975@gmail.com>
2021-11-24 16:00:52 -0300

    Creating images cards

--- JS/AddingElements.js
+++ JS/AddingElements.js
@@ -1,2 +1,5 @@
 //Adding link related to stylesheets
 $documentHead.insertAdjacentElement("afterbegin", $linkToCSS);
+
+//Adding divs fragment
+$documentBody.appendChild($divsFragment);
--- JS/CreatingElements.js
+++ JS/CreatingElements.js
@@ -24,3 +24,41 @@ $linkToCSS.href = "../dist/main.css";
 
 //-----------------------------------------------------------------------
 //BODY
+//Creating Divs
+const $divsFragment = document.createDocumentFragment();
+
+//Assigning images to every card
+const imgReferences = [
+  {
+    img: "https://www.ngenespanol.com/wp-content/uploads/2018/08/Nuevo-museo-en-la-Estatua-de-la-Libertad.jpg",
+    title: "New York",
+  },
+  {
+    img: "https://estaticos.muyhistoria.es/media/cache/1140x_thumb/uploads/images/pyr/57baab765bafe83c45be0a61/paris.jpg",
+    title: "Paris",
+  },
+  {
+    img: "https://elviajerofeliz.com/wp-content/uploads/2017/10/Rutas-por-Londres.jpg",
+    title: "London",
+  },
+];
+//--------------------------------------------------------------
+//CREATING CARDS WITH IMAGES
+for (let i = 0; i < 3; i++) {
+  const $div = document.createElement("div");
+  $div.classList.add("item", "card-component");
+  //Creating images containers (figure)
+  const $imgContainer = document.createElement("figure");
+  $imgContainer.classList.add("img-container");
+  $div.appendChild($imgContainer);
+  //Appending images to imgContainer
+  const $image = document.createElement("img");
+  $image.src = imgReferences[i].img;
+  $image.alt = imgReferences[i].title;
+  $imgContainer.appendChild($image);
+  //Creating info containers
+  const $infoContainer = document.createElement("div");
+  $infoContainer.classList.add("item", "info-container");
+  $div.appendChild($infoContainer);
+  $divsFragment.appendChild($div);
+}
